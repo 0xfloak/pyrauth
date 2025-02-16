9c83aba5467496cafbac1f9213d5fc18ee6ee93c
floak <0xfloak@proton.me>
2025-02-16 09:52:10 -0400

    Implement code verification and URI importing

--- pyrauth.py
+++ pyrauth.py
@@ -1,14 +1,21 @@
 import io
 import argparse
+import readchar
 
 import pyotp
 import qrcode
 from qrcode.image.pure import PyPNGImage
 
 
-def main(name, image_out):
-    key = pyotp.random_base32()
-    uri = pyotp.totp.TOTP(key).provisioning_uri(name=name, issuer_name="pyrauth")
+def main(name, image_out, static_uri):
+    if static_uri:
+        totp = pyotp.parse_uri(static_uri)
+        uri = static_uri
+    else:
+        key = pyotp.random_base32()
+        totp = pyotp.totp.TOTP(key)
+        uri = totp.provisioning_uri(name=name, issuer_name="pyrauth")
+
     print(f'{"#" * 8} ·~ TOTP URL ~· {"#" * (len(uri) - 24)}')
     print(uri)
     print("#" * len(uri))
@@ -24,10 +31,18 @@ def main(name, image_out):
         qrcode.make(uri, image_factory=PyPNGImage).save(image_out)
         print(f"Saved to {image_out}")
 
+    while True:
+        print(f"Verification code: {totp.now()}")
+        print("ENTER for refresh, anything else to exit>")
+
+        if readchar.readchar() != "\n":
+            break
+
 
 def parseargs():
     parser = argparse.ArgumentParser()
     parser.add_argument("name", help="The name to label the TOTP with")
+    parser.add_argument("--uri", help="Use a premade URI", nargs="?")
     parser.add_argument(
         "-o",
         "--output-file",
@@ -57,4 +72,4 @@ if __name__ == "__main__":
     banner()
 
     out_file = args.output_file.name if args.output_file else None
-    main(args.name, out_file)
+    main(args.name, out_file, args.uri)
