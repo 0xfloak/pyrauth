2ee227f129a9b7268fa00c0a245497ab0fa6183e
floak <0xfloak@proton.me>
2025-02-16 09:52:10 -0400

    Basic TOTP generator

---
+++ pyrauth.py
@@ -0,0 +1,52 @@
+import pyotp
+import qrcode
+import io
+import argparse
+
+
+def main(name, image_out):
+    key = pyotp.random_base32()
+    uri = pyotp.totp.TOTP(key).provisioning_uri(name=name, issuer_name="pyrauth")
+    print(f'{"#" * 8} ·~ TOTP URL ~· {"#" * (len(uri) - 24)}')
+    print(uri)
+    print("#" * len(uri))
+
+    qr = qrcode.QRCode()
+    qr.add_data(uri)
+    f = io.StringIO()
+    qr.print_ascii(out=f)
+    f.seek(0)
+    print(f.read())
+
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="The name to label the TOTP with")
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        help="File to write PNG image to",
+        nargs="?",
+        type=argparse.FileType("w"),
+    )
+    return parser.parse_args()
+
+
+def banner():
+    print(
+        """
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░
+░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓████████▓▒░
+░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░   ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░
+"""
+    )
+
+
+if __name__ == "__main__":
+    args = parseargs()
+    banner()
+    main(args.name, args.output_file)
