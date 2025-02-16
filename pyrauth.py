2ecfcb434fac638482e14030187dd7f06087458c
floak <0xfloak@proton.me>
2025-02-16 09:52:10 -0400

    Implement labels for you and friend :)

--- pyrauth.py
+++ pyrauth.py
@@ -7,16 +7,44 @@ import qrcode
 from qrcode.image.pure import PyPNGImage
 
 
-def main(name, image_out, static_uri):
+def main(share_name, local_name, image_out, static_uri, non_interactive):
     if static_uri:
         totp = pyotp.parse_uri(static_uri)
-        uri = static_uri
     else:
         key = pyotp.random_base32()
         totp = pyotp.totp.TOTP(key)
-        uri = totp.provisioning_uri(name=name, issuer_name="pyrauth")
 
-    print(f'{"#" * 8} ·~ TOTP URL ~· {"#" * (len(uri) - 24)}')
+    local_uri = totp.provisioning_uri(name=local_name, issuer_name="PYRauth")
+    share_uri = totp.provisioning_uri(name=share_name, issuer_name="PYRauth")
+
+    print_uri(local_uri, "LOCAL URI (FOR YOU)")
+
+    if not non_interactive:
+        print("Add this to your own authenticator app.")
+        print("Then press any key to view share URI (labeled for friend)")
+        readchar.readchar()
+        print()
+        print()
+
+    print_uri(share_uri, "SHARE URI (FOR FRIEND)")
+
+    if image_out:
+        qrcode.make(share_uri, image_factory=PyPNGImage).save(image_out)
+        print(f"Saved to {image_out}")
+
+    if non_interactive:
+        return
+
+    while True:
+        print(f"Verification code: {totp.now()}")
+        print("ENTER for refresh, anything else to exit>")
+
+        if readchar.readchar() != "\n":
+            break
+
+
+def print_uri(uri, title):
+    print(f'{"#" * 8} ·~ {title} ~· {"#" * (len(uri) - len(title) - 8 - 8)}')
     print(uri)
     print("#" * len(uri))
 
@@ -27,26 +55,19 @@ def main(name, image_out, static_uri):
     f.seek(0)
     print(f.read())
 
-    if image_out:
-        qrcode.make(uri, image_factory=PyPNGImage).save(image_out)
-        print(f"Saved to {image_out}")
-
-    while True:
-        print(f"Verification code: {totp.now()}")
-        print("ENTER for refresh, anything else to exit>")
-
-        if readchar.readchar() != "\n":
-            break
-
 
 def parseargs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("name", help="The name to label the TOTP with")
-    parser.add_argument("--uri", help="Use a premade URI", nargs="?")
+    parser.add_argument("local_name", help="The name to label your own TOTP with")
+    parser.add_argument("share_name", help="The name your friend will see on their TOTP key")
+    parser.add_argument("--uri", help="Use a premade URI for the key", nargs="?")
+    parser.add_argument(
+        "-q", "--non-interactive", help="Don't prompt for input", action="store_true"
+    )
     parser.add_argument(
         "-o",
         "--output-file",
-        help="File to write PNG image to",
+        help="File to write PNG image to (share version)",
         nargs="?",
         type=argparse.FileType("w"),
     )
@@ -72,4 +93,4 @@ if __name__ == "__main__":
     banner()
 
     out_file = args.output_file.name if args.output_file else None
-    main(args.name, out_file, args.uri)
+    main(args.share_name, args.local_name, out_file, args.uri, args.non_interactive)
